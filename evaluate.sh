f893934722b48fe2429615150271fb7ec03d607b
Grazie-567 <ayc22@mails.tsinghua.edu.cn>
2026-08-03 20:29:08 +0800

    Delete evaluate.sh

--- evaluate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-deepspeed --include="localhost:7" \
-    --master_port=29513 \
-    evaluate_wer_new.py \
-    --model_path "/mnt/lv2/FLEURS/whisper-finetune-results/SD/small" \
-    --processor_path "/mnt/lv2/FLEURS/whisper-small" \
-    --save_path "/home/aiyuchen/MultiLang-Finetune/finetune_SD/eval_results/small" \
-    --batch_size 16
